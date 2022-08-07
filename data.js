1cd32ea3b8be151830f7c2e8f7d2abb72f655cb2
schoukah <21149759+schoukah@users.noreply.github.com>
2022-08-07 16:15:43 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,221 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-agora2_entree_080",
+      "name": "agora2_entree_080",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 3.1083508750650477,
+        "pitch": -0.012731455318377272,
+        "fov": 1.3785535375643068
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9147672398760296,
+          "pitch": 0.09505324825272332,
+          "rotation": 12.566370614359176,
+          "target": "1-agora2_lutrin_072"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-agora2_lutrin_072",
+      "name": "Agora2_lutrin_072",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.6009480835095324,
+        "pitch": -0.020951455418112275,
+        "fov": 1.3573407103127428
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2732982419659304,
+          "pitch": 0.05406840319206552,
+          "rotation": 0,
+          "target": "0-agora2_entree_080"
+        },
+        {
+          "yaw": -1.5601919251610727,
+          "pitch": 0.2168743778174651,
+          "rotation": 0,
+          "target": "2-agora2_gauche_071"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-agora2_gauche_071",
+      "name": "Agora2_gauche_071",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 0.31584829366736145,
+        "pitch": -0.011682657098006644,
+        "fov": 1.3785535375643068
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.17917717547294565,
+          "pitch": 0.23231801957074438,
+          "rotation": 0,
+          "target": "3-agora2_chaises_g_074"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-agora2_chaises_g_074",
+      "name": "agora2_chaises_G_074",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.771302114505536,
+        "pitch": 0.06663241143979448,
+        "fov": 1.3785535375643068
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9072058487270196,
+          "pitch": -0.15611506870207492,
+          "rotation": 0,
+          "target": "4-ag2_haut_g_075"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.109362609661641,
+          "pitch": 0.19556528256429218,
+          "title": "Indice!",
+          "text": "Lire un livre, ce n'est pas sortir du monde, mais c'est entrer dans le monde à travers une autre entrée. (<a href=\"https://www.google.com\">Fabrizio Caramagna</a>)<br>"
+        }
+      ]
+    },
+    {
+      "id": "4-ag2_haut_g_075",
+      "name": "Ag2_haut_G_075",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.11097027192057851,
+        "pitch": 0.49501297037271463,
+        "fov": 1.377517843313523
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07029302771145929,
+          "pitch": 0.45183593820682866,
+          "rotation": 6.283185307179586,
+          "target": "3-agora2_chaises_g_074"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.637718592705669,
+          "pitch": 0.26123131217941165,
+          "title": "Indice!",
+          "text": "Chacune de nos lectures laisse une graine qui germe. (<a href=\"https://www.google.com\">Jules Renard</a>)"
+        }
+      ]
+    }
+  ],
+  "name": "AG2",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
